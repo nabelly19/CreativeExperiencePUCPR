904de97772975042c19ebbd21ff5294de17f901d
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-05 18:04:25 -0300

    Muitas alteracoes

--- Dashboard_Web/login.py
+++ Dashboard_Web/login.py
@@ -4,7 +4,24 @@ login = Blueprint("login",__name__, template_folder="templates") #Esta linha cri
 
 users = {
     "user1":"1234",
-    "user2":"12345"
+    "user2":"12345",
+    "user3":"12345",
+    "user4":"12345",
+    "user5":"12345",
+    "user6":"12345",
+    "user7":"12345",
+    "user8":"12345"
+}
+
+adm = {
+    "adm1":"1234",
+    "adm2":"1234",
+    "adm3":"1234",
+    "adm4":"1234",
+    "adm5":"1234",
+    "adm6":"1234",
+    "adm7":"1234",
+    "adm8":"1234"
 }
 
 @login.route('/login_user')
@@ -20,6 +37,8 @@ def validated_user():
         # print(user, password) # Imprime aqui no terminal o "user" e a "passaword" do usuário
         if user in users and users[user] == password: # Esta condição vefica se o "user" inserido pelo usuário é uma das chaves do dicionário e se no dicionário posição "user" ("user" que o usuário digitou) a senha é igual aquela digitada pelo usuário
             return redirect('/dashboard')
+        elif user in adm and user[user] == password:
+            return redirect('/dashboard_adm') # Implementar uma dashboard para adms e outra para usuários normais
         else:
             return '<h1>invalid credentials!</h1>' # Caso não seja, retorna uma página com texto escrito: "invalid credencials"
     else:
@@ -28,25 +47,32 @@ def validated_user():
 
 @login.route('/register_user')
 def register_user():
-    return render_template("addUser.html") # Encaminha o usuário para um formulário
+    return render_template("registerUser.html") # Encaminha o usuário para um formulário
 
 
 @login.route('/add_user', methods=['GET','POST'])
 def add_user():
-    global users # faz uso do dicionário global criado no início do código
+    global users, adm # faz uso do dicionário global criado no início do código
     if request.method == 'POST': # Verifica se o método de comunicação que usuário solicitou é POST
         user = request.form['user'] # Caso seja, define uma variável "user" que recebe o que o usuário inseriu no input de name 'user'
         password = request.form['password'] # Além de criar uma variável "password" que recebe o preenchimento do input de name 'password'
+        confirm_password = request.form['confirm_password'] # Futuramente fazer verificação
+        user_type = request.form['user_type']
+        if user_type == 'user': # Se o usuário a ser cadastrado seja um usuário comum
+            users[user] = password # Salvar no dicionário users
+        elif user_type == 'adm': # Se o usuário a ser cadastrado seja um adm
+            adm[user] = password # Salvar no dicionário adm
     else:
         user = request.args.get('user', None) # Se a solicitação não for ‘POST’ (ou seja, é ‘GET’), estas linhas tentam obter os valores dos parâmetros ‘user’ e ‘password’ da string de consulta da solicitação. Se esses parâmetros não estiverem presentes, eles serão definidos como None.
-        password = request.args.get('password', None) 
-    users[user] = password # Esta linha adiciona o usuário e a senha ao dicionário "users"
-    return render_template("listar_editar_remover", users=users)
+        password = request.args.get('password', None)
+        confirm_password = request.args.get('confirm_password', None)
+        users[user] = password # Esta linha adiciona o usuário e a senha ao dicionário "users"
+    return redirect("/list_users")
     
 @login.route('/list_users')
 def list_users():
-    global users
-    return render_template("listar_editar_remover", users=users)
+    global users, adm
+    return render_template("userList.html", users=users, adm=adm)
 
 @login.route('/remove_user')
 def remove_user():
---
+++ Dashboard_Web/pendencias.md
@@ -0,0 +1,15 @@
+# Futuras Features do Projeto
+
+### Login
+
+* Modal de credenciais inválidas;
+
+### Cadastrar usuário
+
+* No momento de confirmar a senha, fazer uma comparação em relação a senha que foi digitada no input de senha e no input de confirmar a senha, caso sejam diferentes alertar ao usuário;
+
+* IF e ELSE para abrir dashboard com privilégios de admin;
+
+### Lista de usuários
+
+* 
