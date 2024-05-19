3dea18c911c1b9d5f08d9870559617b377559bc6
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-19 01:49:09 -0300

    Add: banco de dados sendo criado
    Função de login se for user admin ok

--- Dashboard_Web/models/db.py
+++ Dashboard_Web/models/db.py
@@ -4,6 +4,6 @@ from datetime import datetime
 db= SQLAlchemy()
 
 bd_name = "FloodPrevention"
-password = "2n#*uB?w!r_O" # Gerar hash dessa senha
+password = "pss" # Gerar hash dessa senha
 
 instance = f"mysql+pymysql://root:{password}@localhost:3306/{bd_name}"
