535e3369ba1502b3711b55ef486e29e9f56aeed1
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-19 14:39:42 -0300

    env

--- Dashboard_Web/models/db.py
+++ Dashboard_Web/models/db.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-db= SQLAlchemy()
 
-bd_name = "FloodPrevention"
-password = "pss" # Gerar hash dessa senha
+load_dotenv()  # Carrega as variáveis do arquivo .env
 
-instance = f"mysql+pymysql://root:{password}@localhost:3306/{bd_name}"
+db = SQLAlchemy()
+
+db_name = os.getenv("DB_NAME")
+db_password = os.getenv("DB_PASSWORD")
+db_user = os.getenv("DB_USER")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+
+instance = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
