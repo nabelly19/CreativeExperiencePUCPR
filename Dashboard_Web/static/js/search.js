7e4d8d636c8a02871b86a9cfe086b4ce02dfb921
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-21 19:44:30 -0300

    Delete function js

--- Dashboard_Web/static/js/search.js
+++ Dashboard_Web/static/js/search.js
@@ -1,18 +1,3 @@
-var input_apagar = document.getElementById("validation");
-var word = "APAGAR";
-
-function request(){
-    if (input_apagar.value == word){
-        //quando true faz requisição
-        console.log("tá podendo");
-        return true;
-    }
-
-    else 
-        console.log("não tá podendo");
-        return false;
-}
-
 function filterCards() {
     const input = document.getElementById('searchInput');
     const filter = input.value.toLowerCase();
