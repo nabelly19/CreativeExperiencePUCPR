fe4e2dd62d52ebf60a55f99b9b4e8860e5f3b303
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-23 16:47:17 -0300

    update: app rodando

--- Dashboard_Web/controllers/read_controller.py
+++ Dashboard_Web/controllers/read_controller.py
@@ -4,9 +4,17 @@ from models.iot.log import Log
 
 read = Blueprint("read",__name__, template_folder="views")
 
+# APAGAR DEPOIS -------------------------------------------
+temperature = 0
+humidity = 0
+mensagem_de_alerta = ""
+alerta_value = 0
+botao_value = 0
+mensagem_nivel_da_agua = ""
+#-----------------------------------------------------------
 
 @read.route('/dashboard')
-@login_required
+#@login_required
 def dashboard():
     global temperature, humidity, mensagem_de_alerta, mensagem_nivel_da_agua, alerta_value
     values = {"Temperatura":temperature, "Umidade":humidity, "Mensagem de alerta":mensagem_de_alerta, "Nível da água":mensagem_nivel_da_agua, "Status do alarme":alerta_value}
