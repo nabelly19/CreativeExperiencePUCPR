948d0b29054bf43bc465420c1a089e8ae445499f
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-23 18:34:33 -0300

    Add: user_controller

---
+++ Dashboard_Web/controllers/user_controller.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from models.user.users import Users
+from models.user.client import Client
+from models.user.admin import Admin
+
+users = Blueprint("users",__name__, template_folder="views")
