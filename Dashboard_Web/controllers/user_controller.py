634e52545b3f7dea4e730805f7b0170cbd1ac8b0
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-29 18:15:06 -0300

    Update: users

--- Dashboard_Web/controllers/user_controller.py
+++ Dashboard_Web/controllers/user_controller.py
@@ -2,6 +2,14 @@ from flask import Blueprint, request, render_template, redirect, url_for, flash
 from models.user.users import Users
 from models.user.client import Client
 from models.user.admin import Admin
+from models.user.role import Role
 
 users = Blueprint("users",__name__, template_folder="views")
+
+@users.route('/register_admin')
+def register_admin():
+    roles = Role.get_all_roles()
+
+    return render_template("registerAdmin.html", roles = roles)
+
 
